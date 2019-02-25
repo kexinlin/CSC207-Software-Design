aaecb757be6c2d5dc07989fce7164a7ba5cf11ef
Songheng Yin <songheng.yin@mail.utoronto.ca>
2019-02-25 16:13:08 -0500

    BankManager

---
+++ phase1/src/atm/BankManager.java
@@ -0,0 +1,16 @@
+public class BankManager{
+    public User createUser(String username, String password){
+        User u = new User();
+        u.username = username;
+        u.password = password;
+        return u;
+    }
+
+    public restockMachine(int faceValue, int number){
+      
+    }
+
+    public undoTransacation(){
+
+    }
+}
