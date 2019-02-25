01c03da574600587cc20b2ba91294f3d9c023a72
ThisTusooa <tusooa@vista.aero>
2019-02-25 15:50:10 -0500

    ATM, Cash: starter code

--- phase1/src/atm/ATM.java
+++ phase1/src/atm/ATM.java
@@ -1,4 +1,25 @@
 package atm;
 
+import java.util.Collection;
+
 public class ATM {
+	/**
+	 * Constructs an instance of ATM.
+	 */
+	public ATM() {
+
+	}
+
+	public boolean login(String username, String password) {
+		return true;
+	}
+
+	public boolean depositCash(Collection<? extends Cash> cash) {
+		return true;
+	}
+
+	public boolean withdrawCash(Collection<? extends Cash> cash) {
+		return true;
+	}
+
 }
