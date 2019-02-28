3b5e5227f00c021a3ba26035f74794272a52f62b
ThisTusooa <tusooa@vista.aero>
2019-02-27 22:04:07 -0500

    *Account: fixed ctors. now they have the correct ctors.

--- phase1/src/atm/CreditCardAccount.java
+++ phase1/src/atm/CreditCardAccount.java
@@ -1,9 +1,19 @@
 package atm;
 
-public class CreditCardAccount extends DebtAccount {
-	public CreditCardAccount() {
+import java.util.Date;
 
+public class CreditCardAccount extends DebtAccount {
+	/**
+	 * Create an instance of account
+	 * @param balance the balance of the account
+	 * @param dateOfCreation the date of creation
+	 * @param accountId account id
+	 * @param owner owner of the account
+	 */
+	public CreditCardAccount(double balance, Date dateOfCreation, String accountId, User owner) {
+		super(balance, dateOfCreation, accountId, owner);
 	}
+
 	public double getBalance() {
 		return 0;
 
--- phase1/src/atm/DebtAccount.java
+++ phase1/src/atm/DebtAccount.java
@@ -1,5 +1,16 @@
 package atm;
 
-public abstract class DebtAccount extends Account {
+import java.util.Date;
 
+public abstract class DebtAccount extends Account {
+	/**
+	 * Create an instance of account
+	 * @param balance the balance of the account
+	 * @param dateOfCreation the date of creation
+	 * @param accountId account id
+	 * @param owner owner of the account
+	 */
+	public DebtAccount(double balance, Date dateOfCreation, String accountId, User owner) {
+		super(balance, dateOfCreation, accountId, owner);
+	}
 }
