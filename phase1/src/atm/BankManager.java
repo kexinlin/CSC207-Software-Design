34ae61224d19038e6a6a80ce135dd7bf0aab065c
Songheng Yin <songheng.yin@mail.utoronto.ca>
2019-02-28 20:18:07 -0500

    update Manager

--- phase1/src/atm/BankManager.java
+++ phase1/src/atm/BankManager.java
@@ -1,6 +1,8 @@
 package atm;
+import java.util.ArrayList;
 
 public class BankManager implements Loginable {
+	private ArrayList<User> users;	// stores all the users.
 	/**
 	 * Check if the password provided is the same as the one set for the user.
 	 * @param password the password to check.
@@ -11,20 +13,36 @@ public class BankManager implements Loginable {
 		return false;
 	}
 
-    public User createUser(String login, String password){
+	public BankManager(){
+		this.users = new ArrayList<User>();
+	}
+
+	public User createUser(String login, String password){
         User u = new User(login, password);
+        this.users.add(u);
         return u;
     }
 
-    public Account createAccount(){
+    public Object responseToRequest(){
+		// input: Accept the request of creating the account or not
+		if (false)
+			return false;	// if not
+		else{
+			return createAccount();
+		}
+	}
 
+	public Account createAccount(String accountType){
+		if(accountType == "CreditCardsAccount"){
+			//
+		}
 	}
 
     public boolean restockMachine(int faceValue, int number){
 		return false;
     }
 
-    public boolean undoTransacation(){
+    public boolean undoTransaction(){
 		return false;
     }
 }
