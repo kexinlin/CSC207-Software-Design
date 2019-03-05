bf330e90448d0b93adc4becd2bb9866ea4ec36c7
Xiao Li <shirleyxx.li@mail.utoronto.ca>
2019-03-05 01:18:38 -0500

    Add transaction class constructor

--- phase1/src/atm/Transaction.java
+++ phase1/src/atm/Transaction.java
@@ -4,6 +4,7 @@ public class Transaction {
 	private Account source;
 	private Account destination;
 	private float amount;
+	
 	public Transaction(Account source, Account destination, float amount) {
 		this.source = source;
 		this.destination = destination;
