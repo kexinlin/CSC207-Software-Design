8541788491322eae1398104e637913356b54b282
ThisTusooa <tusooa@vista.aero>
2019-03-08 10:11:40 -0500

    CashController: bugfix

--- phase1/src/controller/transactions/CashController.java
+++ phase1/src/controller/transactions/CashController.java
@@ -19,7 +19,6 @@ import java.util.Map;
  */
 public class CashController {
 	private ATM machine;
-	private ArrayList<HashMap<Cash, Integer>>
 
 	/**
 	 * Constructs a cash controller.
@@ -39,6 +38,7 @@ public class CashController {
 		/*while (running) {
 			if (workingCashType.getNumVal() <= )
 		}*/
+		return cashToWithdraw;
 	}
 
 	/**
