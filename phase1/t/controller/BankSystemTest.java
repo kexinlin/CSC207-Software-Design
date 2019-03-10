920eea0005882f4fcbb51aa96090426ca91e4197
ThisTusooa <tusooa@vista.aero>
2019-03-09 21:41:03 -0500

    BankSystemTest: test gain interest

--- phase1/t/controller/BankSystemTest.java
+++ phase1/t/controller/BankSystemTest.java
@@ -1,27 +1,42 @@
 package controller;
 
+import model.accounts.Account;
+import model.exceptions.AccountNotExistException;
+import model.persons.Loginable;
+import model.persons.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.*;
+import java.util.Calendar;
 
 import static org.junit.Assert.*;
 
 public class BankSystemTest {
 	private BankSystem bankSystem;
-	private String recordFileName = "test-rec.txt";
+	private String recordFileName = "records.test.txt";
 
 	@Before
 	public void setUp() {
 		File file = new File(recordFileName);
-
-		bankSystem = new BankSystem(recordFileName);
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			writer.write(
+				"manager,mgr1,lolol\n" +
+					"user,Foo Bar,u1,xxx\n" +
+					"account,chq,45.0,1552098056134,127,u1\n" +
+					"account,sav,100.0,1552172989694,874637,u1\n" +
+					"set,primary-acc,u1,127");
+			writer.close();
+			bankSystem = new BankSystem(recordFileName);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
 	}
 
 	@After
 	public void cleanUp() {
-		bankSystem.close();
 	}
 
 	@Test
@@ -39,5 +54,24 @@ public class BankSystemTest {
 
 	@Test
 	public void testPayBill() {
+	}
+
+	@Test
+	public void testGainInterest() {
+		Calendar.Builder builder = new Calendar.Builder();
+		// the end of February
+		builder.setDate(2019, Calendar.FEBRUARY,28);
+		Calendar cal = builder.build();
+		System.out.println(cal.getTime());
+		bankSystem.setCurrentTime(cal.getTime());
+		bankSystem.close();
+		try {
+			Account acc = bankSystem.getAccountById("874637");
+			assertNotNull(acc);
+			// it should gain 0.1% interest.
+			assertEquals(100.1, acc.getBalance(), 0.0001);
+		} catch (AccountNotExistException e) {
+			fail("Cannot find the account.");
+		}
 	}
 }
