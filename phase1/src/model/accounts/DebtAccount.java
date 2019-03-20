83e1b5b2a6d5f487fd540ac40fe89835dd7ae7f2
Songheng Yin <songheng.yin@mail.utoronto.ca>
2019-03-20 19:58:55 -0400

    class DebtAccount: add field limit

--- phase1/src/model/accounts/DebtAccount.java
+++ phase1/src/model/accounts/DebtAccount.java
@@ -6,15 +6,18 @@ import model.persons.User;
 import java.util.Date;
 
 public abstract class DebtAccount extends Account {
+	protected double limit;	// the maximum amount of debt you can incur.
 	/**
 	 * Create an instance of account
 	 * @param balance the balance of the account
 	 * @param dateOfCreation the currentTime of creation
 	 * @param accountId account id
 	 * @param owner owner of the account
+	 * @param limit the maximum amount of debt you can incur.
 	 */
-	DebtAccount(double balance, Date dateOfCreation, String accountId, User owner) {
+	DebtAccount(double balance, Date dateOfCreation, String accountId, User owner, double limit) {
 		super(balance, dateOfCreation, accountId, owner);
+		this.limit = limit;
 	}
 
 	/**
