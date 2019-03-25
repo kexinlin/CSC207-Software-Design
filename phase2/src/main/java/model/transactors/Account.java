35975a27848af14f2a40338d592c3a85487f73a4
Songheng Yin <songheng.yin@mail.utoronto.ca>
2019-03-25 16:02:51 -0400

    class Account: add return type

--- phase2/src/main/java/model/transactors/Account.java
+++ phase2/src/main/java/model/transactors/Account.java
@@ -145,4 +145,14 @@ public abstract class Account extends Transactor {
 	 * @return the balance factor of this account
 	 */
 	public abstract int balanceFactor();
+
+	/**
+	 *
+	 * @return the AccountID
+	 */
+	public String getAccountId(){
+		return accountId;
+	}
+
+	public abstract String getAccountType();
 }
