4d36f590ca1cbc165ecc589106d52d193d08e5f2
ThisTusooa <tusooa@vista.aero>
2019-03-29 20:58:59 -0400

    LoginCtrl: add usability. press enter to focus password/login.

--- phase2/src/main/java/view/GUI/GUIController.java
+++ phase2/src/main/java/view/GUI/GUIController.java
@@ -1,12 +1,27 @@
 package view.GUI;
 
-public abstract class GUIController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public abstract class GUIController implements Initializable {
+	/// register event handlers here
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+
+	}
 	GUIManager guiManager;
 
 	public void setGUIManager(GUIManager guiManager) {
 		this.guiManager = guiManager;
 	}
 
-
+	protected void showAlert(Alert.AlertType type, String content, String title) {
+		Alert alert = new Alert(type);
+		alert.setContentText(content);
+		alert.setHeaderText(title);
+		alert.show();
+	}
 }
--- phase2/src/main/java/view/GUI/LoginController.java
+++ phase2/src/main/java/view/GUI/LoginController.java
@@ -4,33 +4,62 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import model.persons.User;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class LoginController extends GUIController{
+public class LoginController extends GUIController {
 
 	@FXML
 	TextField usernameInput;
 	@FXML
 	PasswordField passwordInput;
-
 	@FXML
-	public void loginButtonOnClick(ActionEvent actionEvent) throws IOException {
+	Button loginButton;
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		usernameInput.setOnKeyReleased(this::slotUsernameInputKeyReleased);
+		passwordInput.setOnKeyReleased(this::slotPasswordInputKeyReleased);
+		loginButton.setOnMouseClicked(this::slotLoginButtonMouseClicked);
+	}
+
+	private void slotUsernameInputKeyReleased(KeyEvent event) {
+		if (event.getCode().equals(KeyCode.ENTER)) {
+			passwordInput.requestFocus();
+		}
+	}
+
+	private void slotPasswordInputKeyReleased(KeyEvent event) {
+		if (event.getCode().equals(KeyCode.ENTER)) {
+			loginUser();
+		}
+	}
+
+	private void slotLoginButtonMouseClicked(MouseEvent ignore) {
+		loginUser();
+	}
+
+	private void loginUser() {
 		String username = usernameInput.getText();
 		String password = passwordInput.getText();
 		System.out.println(username);
 		System.out.println(password);
 
 		if (username.equals("") || password.equals("")) {
-			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setContentText("Please enter your username or password");
-			alert.setHeaderText("Login failed");
-			alert.show();
+			showAlert(Alert.AlertType.ERROR,
+			"Please enter your username or password",
+			"Login failed");
 		} else if (guiManager.getATM().login(username, password)) {
 			String resourceName;
 			// FIXME must also do UserEmployee's page.
@@ -41,15 +70,21 @@ public class LoginController extends GUIController{
 				resourceName = "/BankManagerHomeScene.fxml";
 			}
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(resourceName));
-			AnchorPane homeScene = loader.load();
+			try {
+				AnchorPane homeScene = loader.load();
 
-			GUIHomeController homeController = loader.getController();
-			homeController.setGUIManager(guiManager);
-			homeController.setCurrentUser(guiManager.bankSystem.getLoginable(username));
+				GUIHomeController homeController = loader.getController();
+				homeController.setGUIManager(guiManager);
+				homeController.setCurrentUser(guiManager.bankSystem.getLoginable(username));
 
-			homeController.show();
+				homeController.show();
 
-			GraphicsUI.root.getChildren().setAll(homeScene);
+				GraphicsUI.root.getChildren().setAll(homeScene);
+			} catch (IOException e) {
+				showAlert(Alert.AlertType.ERROR,
+					"Cannot load your home page",
+					"System error");
+			}
 		} else {
 			Alert alert = new Alert(Alert.AlertType.ERROR);
 			alert.setContentText("Incorrect password. Please check your password again.");
